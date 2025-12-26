3ee66272212af6e4848843ee0032ff89abaf69da
PARTHK-05 <parthkhandelwal489@gmail.com>
2025-12-26 13:07:06 +0530

    update in the innegest.js

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -12,9 +12,13 @@ const syncUser = inngest.createFunction(
 
         const {id , email_addresses , first_name , last_name , image_url} = event.data
 
+        if (!email_addresses?.length) {
+             throw new Error("No email found in Clerk event");
+        }
+
         const newUser = {
             clerkId:id,
-            email:email_addresses[0]?.email_addresses,
+            email: email_addresses[0]?.email_address,
             name:`${first_name || ""} ${last_name || ""}`,
             profilePic:image_url
         }
