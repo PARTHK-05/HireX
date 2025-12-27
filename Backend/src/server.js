42616a81efcb9b244a3f3711ca45ab0a5fa886ed
PARTHK-05 <parthkhandelwal489@gmail.com>
2025-12-27 15:04:56 +0530

    changes in server.js port

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -78,7 +78,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`✅ Server running on port ${PORT}`);
     });
   } catch (error) {
